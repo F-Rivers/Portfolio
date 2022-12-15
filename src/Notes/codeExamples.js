69cc6ccd9ed3f8871eb0be7e17725f2fd2b250a6
F-Rivers <fxjxrx@gmail.com>
2022-12-15 15:13:05 -0800

    adding space/time example

--- src/Notes/codeExamples.js
+++ src/Notes/codeExamples.js
@@ -1,12 +1,12 @@
 //* The syntax for recursive function is:
 
-function recurse() {
-	// function code
-	recurse();
-	// function code
-}
+// function recurse() {
+// 	// function code
+// 	recurse();
+// 	// function code
+// }
 
-recurse();
+// recurse();
 // ------>OUTPUT:
 // RangeError: Maximum call stack size exceeded at recurse (/Users/franciscorivera/Desktop/portfolio/src/Notes/codeExamples.js:5:2)
 
@@ -40,9 +40,7 @@ countDown(4);
 function factorial(x) {
 	if (x === 0) {
 		return 1;
-	}
-	// if number is positive
-	else {
+	} else {
 		return x * factorial(x - 1);
 	}
 }
@@ -55,10 +53,33 @@ if (num > 0) {
 }
 
 // Logic of 'function factorial':
-// factorial(3) returns 3 * factorial(2)
-// factorial(2) returns 3 * 2 * factorial(1)
-// factorial(1) returns 3 * 2 * 1 * factorial(0)
-// factorial(0) returns 3 * 2 * 1 * 1
+// factorial(3) returns 3 * factorial(2) = 6
+// factorial(2) returns 3 * 2 * factorial(1) = 6
+// factorial(1) returns 3 * 2 * 1 * factorial(0) = 6
+// factorial(0) returns 3 * 2 * 1 * 1 = 6
 
 // ------> OUTPUT:
 // The factorial of 3 is 6
+//! -------------------------------------
+//* Example: Size of stacks
+
+function sum(n) {
+	if (n <= 0) return 0;
+	return n + sum(n - 1);
+}
+const n = 5;
+if (n > 0) {
+	let nresult = sum(n);
+	console.log(`The answer is ${nresult}`);
+}
+
+// Logic of 'function sum':
+//  Sum(5)
+// ->sum(4) returns 5 + 4 = 9
+// ->sum(3) returns 9 + 3 = 12
+// ->sum(2) returns 12 + 2 = 14
+// ->sum(1) returns 14 + 1 = 15
+// ->sum(0) returns 15 + 0 = 15
+
+// ------> OUTPUT:
+// The answer is 15
