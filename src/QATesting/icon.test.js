4ebc5d2ecd5f4ba66344258e3c3746e733b746e8
F-Rivers <fxjxrx@gmail.com>
2022-12-26 12:27:56 -0800

    Cant find module from <name>'QATesting/icon.test'

--- src/QATesting/icon.test.js
+++ src/QATesting/icon.test.js
@@ -1,10 +1,19 @@
-import LaptopMacIcon from "@mui/icons-material/LaptopMac";
+import ResponsiveAppBar from "./App.js";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
+// import Fetch from "././fetch";
 
 <svg data-testid='LaptopMacIcon'></svg>;
 
-describes("IconTest",()=>{
-    it("renders Left arrow",()=>{
-    const {container} = renders(<App open={true}/>);
-    expect(container.querySelector(".MuiSvgIcon-root").toBeTruthy()
-})
-})
+test("loads and displays icon and logo", async () => {
+	// ARRANGE
+	render(<ResponsiveAppBar url='/.' />);
+
+	// ACT
+	await userEvent.click(screen.getByText("RIVERA"));
+	await screen.findByRole("heading");
+
+	// ASSERT
+	expect(screen.getByRole("heading")).toHaveTextContent("RIVERA");
+});
