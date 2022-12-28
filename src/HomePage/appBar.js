48a82c20b4d98d71f23b97fcbcd2034274e43c3e
F-Rivers <fxjxrx@gmail.com>
2022-12-28 09:57:07 -0800

    need to create objs from array

--- src/HomePage/appBar.js
+++ src/HomePage/appBar.js
@@ -14,8 +14,10 @@ import MenuItem from "@mui/material/MenuItem";
 // import AdbIcon from "@mui/icons-material/Adb";
 import LaptopMacIcon from "@mui/icons-material/LaptopMac";
 
+// ! need to create objects from arrays
 const pages = ["GitHub", "LinkedIn", "Projects"];
 const settings = ["About Me", "Contact Me", "FAQ"];
+//!------------------------------------
 
 function ResponsiveAppBar() {
 	const [anchorElNav, setAnchorElNav] = React.useState(null);
