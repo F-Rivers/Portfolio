1a9205fd8e6932c653355fc7ee999036d8846561
F-Rivers <fxjxrx@gmail.com>
2023-05-12 14:28:32 -0700

    changed app.test

--- src/QATesting/App.test.js
+++ src/QATesting/App.test.js
@@ -1,6 +1,9 @@
 import { render, screen } from "@testing-library/react";
 import App from "../App";
+import * as React from "react";
 
-render(<App />);
-
-const renderApp = screen.getAllByAltText("alt");
+test("renders an app", () => {
+	render(<App />);
+	const appHeader = screen.getBy("header");
+	expect("appHeader").toHaveValue();
+});
